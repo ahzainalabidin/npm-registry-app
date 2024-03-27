7b245c29ee0ec26ad6b5dd8a50e6dc29ae2bb12b
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 10:27:47 +0800

    Fetch data from NPM registry API based on search term

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,25 @@ const router = createBrowserRouter([
       },
       {
         path: '/search',
-        element: <SearchPage />
+        element: <SearchPage />,
+        loader: async ({ request }) => {
+
+          const { searchParams } = new URL(request.url);
+          const term = searchParams.get('term');
+
+          if (!term) {
+            throw new Error('Search term must be provided');
+          }
+
+          const res = await fetch(
+            `https://registry.npmjs.com/-/v1/search?text=${term}`
+          );
+
+          const data = await res.json();
+
+          return data.objects;
+
+        },
       },
       {
         path: '/packages/:name',
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,3 +1,7 @@
+import { useLoaderData } from "react-router-dom";
+
 export default function SearchPage() {
+    const data = useLoaderData();
+    console.log(data);
     return <div>Search Page</div>;
 }
