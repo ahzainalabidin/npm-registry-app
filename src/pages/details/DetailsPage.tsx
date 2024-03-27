8f2f492a8dd0ff8b465556a4e15c3958b9526ff9
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 14:39:00 +0800

    Improve type safety between the page and loader

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,3 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+import { DetailsLoaderResult } from "./detailsLoader";
+
 export default function DetailsPage() {
-    return <div>Details Page</div>;
+
+    const { details } = useLoaderData() as DetailsLoaderResult;
+
+    return (
+        <div>
+            {details.name} - {details.description}
+        </div>
+    );
+
 }
--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,7 +1,12 @@
 import { Params } from "react-router-dom";
 import { getPackage } from "../../api/queries/getPackage";
+import { PackageDetails } from "../../api/types/packageDetails";
 
-export async function detailsLoader({ params }: { params: Params }) {
+export interface DetailsLoaderResult {
+    details: PackageDetails;
+}
+
+export async function detailsLoader({ params }: { params: Params }): Promise<DetailsLoaderResult> {
 
     const { name } = params;
 
