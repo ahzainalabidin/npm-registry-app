3cea8256094d0785f2fd3be4a04c8185c6b99282
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 09:05:36 +0800

    Add header, pages and root layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: '/search',
+        element: <SearchPage />
+      },
+      {
+        path: '/packages/:name',
+        element: <DetailsPage />
+      },
+    ]
+  }
+]);
+
 function App() {
-
-  return (
-    <div>
-      Hi there!
-    </div>
-  );
-
+  return <RouterProvider router={router} />;
 }
 
 export default App;
