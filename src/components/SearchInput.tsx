a33b6512b3d66c8e83c18a353db61bd71b5af325
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 09:31:43 +0800

    Implement programmatic navigation for search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchInput() {
 
     const [term, setTerm] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        navigate(`/search?term=${term}`);
     };
 
     return (
