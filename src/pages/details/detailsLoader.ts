6b2a857c2c2e7627f3f912edbab29a92bd0f6fd8
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 14:26:23 +0800

    Get package based on package name

--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,3 +1,18 @@
-export async function detailsLoader() {
-    return 'data!!';
+import { Params } from "react-router-dom";
+import { getPackage } from "../../api/queries/getPackage";
+
+export async function detailsLoader({ params }: { params: Params }) {
+
+    const { name } = params;
+
+    if (!name) {
+        throw new Error('Name must be provided');
+    }
+
+    const details = await getPackage(name);
+
+    return {
+        details
+    };
+
 }
