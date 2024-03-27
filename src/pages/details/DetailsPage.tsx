692a1dd878e99b3ee56d9e3167f682fb7b2df648
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-27 14:47:12 +0800

    Display details of package

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -6,8 +6,27 @@ export default function DetailsPage() {
     const { details } = useLoaderData() as DetailsLoaderResult;
 
     return (
-        <div>
-            {details.name} - {details.description}
+        <div className="space-y-4">
+
+            <h1 className="text-3xl font-bold my-4">
+                {details.name}
+            </h1>
+
+            <div>
+                <h3 className="text-lg font-bold">Description</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.description}</div>
+            </div>
+
+            <div>
+                <h3 className="text-lg font-bold">License</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.license}</div>
+            </div>
+
+            <div>
+                <h3 className="text-lg font-bold">Author</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.author?.name}</div>
+            </div>
+
         </div>
     );
 
